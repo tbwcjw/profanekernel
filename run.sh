cdcd043d15ce5d3711dedd5ea7bc6fe9cca78426
tbwcjw <35520616+tbwcjw@users.noreply.github.com>
2024-10-03 15:49:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 python3 -m venv venv                        #sets up venv   
 source venv/bin/activate            
-export GH_TOKEN=$GH_TOKEN               #ensure GH_TOKEN is in bashrc
+export GH_TOKEN=$GH_TOKEN                   #ensure GH_TOKEN is in bashrc first
 
-echo "TOKEN $GH_TOKEN"
+if [ -z "$token" ]; then
+    echo "GH_TOKEN required"
+    exit 1
+fi
+echo "TOKEN: $GH_TOKEN"
 
 git pull --strategy-option theirs origin main
 
