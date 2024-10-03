0179545c71a5257af2c5ec41fb86c2673bd21a10
tbwcjw <35520616+tbwcjw@users.noreply.github.com>
2024-10-02 22:06:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 python3 -m venv venv                        #sets up venv   
 source venv/bin/activate            
+export GH_TOKEN=$GH_TOKEN               #ensure GH_TOKEN is in bashrc
 
-git pull                                    #ensure up-to-date
+
+git pull origin main                        #ensure up-to-date
 if [ -f requirements.txt ]; then
     pip install -r requirements.txt         #install requirements
 fi
@@ -14,9 +16,7 @@ wait $!                                     #wait for end of execution
 if [[ $(git status --porcelain) ]]; then    #commit to repo
     git add .
     git commit -m "Monthly update: $(date +'%Y-%m-%d')"
-    export GH_TOKEN=$GH_TOKEN               #ensure GH_TOKEN is in bashrc
-    gh pr create --fill --base main --head main --title "Monthly update: $(date +'%Y-%m-%d')" --body "Automated update."
-    gh pr merge --auto
+    git push origin main
 fi
 
 deactivate                                  #exit venv
