8eae6484cc81e49b637d342d608b77541f473539
tbwcjw <35520616+tbwcjw@users.noreply.github.com>
2024-10-03 15:50:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ python3 -m venv venv                        #sets up venv
 source venv/bin/activate            
 export GH_TOKEN=$GH_TOKEN                   #ensure GH_TOKEN is in bashrc first
 
-if [ -z "$token" ]; then
+if [ -z "$GH_TOKEN" ]; then
     echo "GH_TOKEN required"
     exit 1
 fi
